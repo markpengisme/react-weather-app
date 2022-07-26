68a904e63e4baa711b1bdf2756bad53a26d69903
MarkPeng <markpengisme@gmail.com>
2022-07-27 05:44:43 +0800

    openweather api

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -3,23 +3,25 @@ import { AsyncPaginate } from "react-select-async-paginate"
 import { GEO_API_URL, geoApiOptions } from "../../api"
 
 const Search = ({ onSearchChange }) => {
-
-    const [ search, setSearch ] = useState(null)
+    const [search, setSearch] = useState(null)
 
     const loadOptions = (inputValue) => {
-        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
-        .then(response => response.json())
-        .then(response => {
-            return {
-                options: response.data.map((city) => {
-                    return {
-                        value: `${city.latitude} ${city.longitude}`,
-                        label: `${city.name}, ${city.countryCode}`
-                    }
-                })
-            }
-        })
-        .catch(err => console.error(err))
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+            geoApiOptions
+        )
+            .then((response) => response.json())
+            .then((response) => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    }),
+                }
+            })
+            .catch((err) => console.error(err))
     }
 
     const handleOnChange = (searchData) => {
@@ -29,7 +31,7 @@ const Search = ({ onSearchChange }) => {
 
     return (
         <AsyncPaginate
-            placeholder='Search for city'
+            placeholder="Search for city"
             debounceTimeout={600}
             value={search}
             onChange={handleOnChange}
